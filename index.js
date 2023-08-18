83728e00e68a934edd59c6f4aa5766e3d8f774cf
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-08-17 20:35:05 -0500

    added question prompts for the users README. including types, names, and messages

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your README?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your new README file: ',
+      },
+      {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'Installation Instructions: ',
+      },
+      {
+        type: 'input',
+        name: 'usage information',
+        message: 'Please insert README usage information: ',
+      },
+      {
+        type: 'input',
+        name: 'contribution guidelines',
+        message: 'Please enter your contribution guidelines: ',
+      },
+      {
+        type: 'input',
+        name: 'test instuctions',
+        message: 'Please enter your testing instructions: ',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose the type of license you would like to use: ',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter GitHub username: ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter a valid email address: ',
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
